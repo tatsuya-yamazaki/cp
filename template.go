b0cb10c5bd16ddf130a4515d7ae31f22c1463744
Tatsuya Yamazaki <tatsuya.yamazaki.curry@gmail.com>
2022-01-30 16:38:03 +0900

    template prototype

--- template.go
+++ template.go
@@ -1,31 +1,47 @@
 package main
 
 import(
-	"fmt"
 	"os"
 	"bufio"
 	"strconv"
 )
 
-type IOUtil struct {
-	Scanner bufio.Scanner
+func main() {
 }
 
+type IOUtil struct {
+	Scanner *bufio.Scanner
+}
 func NewIOUtil() *IOUtil {
 	iou := IOUtil{
-		Scanner: bufio.NewScanner(os.Stdin)
+		Scanner: bufio.NewScanner(os.Stdin),
 	}
 	iou.Scanner.Split(bufio.ScanWords)
 	return &iou
 }
-
 func (*IOUtil) ToInt(s string) int {
 	ret, _ := strconv.Atoi(s)
 	return ret
 }
-
 func (iou *IOUtil) Int() int {
+	iou.Scanner.Scan()
+	return iou.ToInt(iou.Scanner.Text())
 }
-
-func main() {
+func (iou *IOUtil) Str() string {
+	iou.Scanner.Scan()
+	return iou.Scanner.Text()
 }
+func (iou *IOUtil) I() int {
+	return iou.Int()
+}
+func (iou *IOUtil) S() string {
+	return iou.Str()
+}
+func (iou *IOUtil) Is(n int) []int {
+	ret := make([]int, n)
+	for i:=0; i<n; i++ {
+		ret[i] = Int()
+	}
+	return ret
+}
+var iou = NewIOUtil()
