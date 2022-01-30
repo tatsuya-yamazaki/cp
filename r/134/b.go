218e9d643782791ffb380b4c3d6df334e7e310e2
Tatsuya Yamazaki <tatsuya.yamazaki.curry@gmail.com>
2022-01-30 16:03:26 +0900

    regular 134 b upperbound

--- r/134/b.go
+++ r/134/b.go
@@ -11,29 +11,57 @@ func main() {
 	fmt.Scan(&n)
 	fmt.Scan(&s)
 	runes := []rune(s)
+	alf := make([][]int, 26)
+	for i, v := range runes {
+		alf[v-'a'] = append(alf[v-'a'], i)
+	}
 
 	l, r := 0, len(s)-1
 	for l < r {
-		nr := r
-		target := -1
-		for l < nr {
-			if runes[l] > runes[nr] && (target == -1 || runes[nr] < runes[target]) {
-				target = nr
+		for i, pos := range alf {
+			if i >= int(runes[l]-'a') {
+				break
 			}
-			nr--
+			if len(pos) == 0 {
+				continue
+			}
+			upper := UpperBound(pos, r)
+			targetPos := -1
+			if pos[upper] <= r {
+				targetPos = upper
+			} else if pos[upper] > r && upper != 0 {
+				targetPos = upper - 1
+			} else {
+				continue
+			}
+			target := pos[targetPos]
+			if l < target && target <= r {
+				runes[l], runes[target] = runes[target], runes[l]
+				r = target - 1
+				alf[i] = alf[i][:targetPos]
+				break
+			}
 		}
-
-		if target > -1 {
-			runes[l], runes[target] = runes[target], runes[l]
-			r = target
-		}
-
 		l++
 	}
+
 	ans := ""
 	for _, v := range runes {
 		ans += string(v)
 	}
 
 	fmt.Println(ans)
+}
+
+func UpperBound(s []int, value int) int {
+        l, r := 0, len(s)-1
+        for l != r {
+                m := (l + r) / 2
+                if value < s[m] {
+			r = m
+                } else {
+			l = m + 1
+                }
+        }
+        return l
 }
