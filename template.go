295c1cf8ef4a78b7658df5f3d206caccbc23197f
Tatsuya Yamazaki <tatsuya.yamazaki.curry@gmail.com>
2022-01-31 19:51:10 +0900

    Max Min functions are added

--- template.go
+++ template.go
@@ -17,6 +17,18 @@ func main() {
 	iou.Pl(a)
 }
 
+func Max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func Min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
 type IOUtil struct {
 	Scanner *bufio.Scanner
 	Reader *bufio.Reader
