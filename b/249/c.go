8ef2960d67972a79a07b9f3ffad04523fc4238ad
Tatsuya Yamazaki <tatsuya.yamazaki.curry@gmail.com>
2022-04-23 23:25:39 +0900

    b 249

---
+++ b/249/c.go
@@ -0,0 +1,151 @@
+package main
+
+import(
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	defer iou.Fl()
+
+	n := iou.I()
+	k := iou.I()
+	var ss [][]rune
+	for i:=0; i<n; i++ {
+		ss = append(ss, []rune(iou.S()))
+	}
+
+	ans := 0
+	for _, is := range BitAll(n) {
+		m := make(map[rune]int)
+		for _, i := range is {
+			for _, r := range ss[i] {
+				m[r]++
+			}
+		}
+		c := 0
+		for _, v := range m {
+			if v == k {
+				c++
+			}
+		}
+		ans = Max(ans, c)
+	}
+
+	iou.Pl(ans)
+}
+
+func BitAll(n int) [][]int {
+        ret := make([][]int, 1<<n)
+        for i:=0; i<(1<<n); i++ {
+                var r []int
+                for j:=0; j<n; j++ {
+                        if i & (1<<j) > 0 {
+                                r = append(r, j)
+                        }
+                }
+                ret[i] = r
+        }
+        return ret
+}
+
+func Max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func Min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+type IOUtil struct {
+	Scanner *bufio.Scanner
+	Reader *bufio.Reader
+	Writer *bufio.Writer
+}
+func NewIOUtil() *IOUtil {
+	iou := IOUtil{
+		Scanner: bufio.NewScanner(os.Stdin),
+		Writer: bufio.NewWriter(os.Stdout),
+	}
+	iou.Scanner.Split(bufio.ScanWords)
+	iou.Scanner.Buffer(make([]byte, 1024), math.MaxInt64)
+	return &iou
+}
+func (*IOUtil) ToInt(s string) int {
+	ret, _ := strconv.Atoi(s)
+	return ret
+}
+func (iou *IOUtil) Int() int {
+	iou.Scanner.Scan()
+	return iou.ToInt(iou.Scanner.Text())
+}
+func (iou *IOUtil) Str() string {
+	iou.Scanner.Scan()
+	return iou.Scanner.Text()
+}
+func (iou *IOUtil) I() int {
+	return iou.Int()
+}
+func (iou *IOUtil) S() string {
+	return iou.Str()
+}
+func (iou *IOUtil) Ints(n int) []int {
+	ret := make([]int, n)
+	for i:=0; i<n; i++ {
+		ret[i] = iou.Int()
+	}
+	return ret
+}
+func (iou *IOUtil) Is(n int) []int {
+	return iou.Ints(n)
+}
+func (iou *IOUtil) Ints2(n int) (a, b []int) {
+        a = make([]int, n)
+        b = make([]int, n)
+        for i:=0; i<n; i++ {
+                a[i] = iou.I()
+                b[i] = iou.I()
+        }
+	return a, b
+}
+func (iou *IOUtil) Is2(n int) (a, b []int) {
+	return iou.Ints2(n)
+}
+func (iou *IOUtil) CumulativeSum(n int) (cumulative, ints []int) {
+	cumulative = append(cumulative, 0)
+	for i:=0; i<n; i++ {
+		ai := iou.Int()
+		cumulative = append(cumulative, cumulative[i] + ai)
+		ints = append(ints, ai)
+	}
+	return
+}
+func (iou *IOUtil) Cms(n int) (cumulative, ints []int) {
+	return iou.CumulativeSum(n)
+}
+func (iou *IOUtil) Print(a ...interface{}) {
+	fmt.Fprint(iou.Writer, a...)
+}
+func (iou *IOUtil) P(a ...interface{}) {
+	iou.Print(a...)
+}
+func (iou *IOUtil) Println(a ...interface{}) {
+	fmt.Fprintln(iou.Writer, a...)
+}
+func (iou *IOUtil) Pl(a ...interface{}) {
+	iou.Println(a...)
+}
+func (iou *IOUtil) Flush() {
+	iou.Writer.Flush()
+}
+func (iou *IOUtil) Fl() {
+	iou.Flush()
+}
+var iou = NewIOUtil()
