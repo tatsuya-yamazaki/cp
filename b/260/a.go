df34395a7cc0374dea0023cd37d2c43916359ea0
Tatsuya Yamazaki <tatsuya.yamazaki.curry@gmail.com>
2022-07-17 22:49:13 +0900

    b 260

---
+++ b/260/a.go
@@ -0,0 +1,161 @@
+package main
+
+import(
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	defer iou.fl()
+
+	s := []rune(iou.s())
+	m := make(map[rune]int)
+	for _, v := range s {
+		m[v]++
+	}
+
+	for k, v := range m {
+		if v == 1 {
+			iou.pl(string(k))
+			return
+		}
+	}
+	iou.pl(-1)
+}
+
+func Pow(x, n int) int {
+	ret := 1
+	for i:=0; i<n; i++ {
+		ret *= x
+	}
+	return ret
+}
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+func Max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func Min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+type ioUtil struct {
+	scanner *bufio.Scanner
+	reader *bufio.Reader
+	writer *bufio.Writer
+}
+func newIOUtil() *ioUtil {
+	iou := ioUtil{
+		scanner: bufio.NewScanner(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+	iou.scanner.Split(bufio.ScanWords)
+	iou.scanner.Buffer(make([]byte, 1024), math.MaxInt64)
+	return &iou
+}
+func (*ioUtil) toInt(s string) int {
+	ret, _ := strconv.Atoi(s)
+	return ret
+}
+func (iou *ioUtil) int() int {
+	iou.scanner.Scan()
+	return iou.toInt(iou.scanner.Text())
+}
+func (iou *ioUtil) str() string {
+	iou.scanner.Scan()
+	return iou.scanner.Text()
+}
+func (iou *ioUtil) i() int {
+	return iou.int()
+}
+func (iou *ioUtil) s() string {
+	return iou.str()
+}
+func (iou *ioUtil) ints(n int) []int {
+	ret := make([]int, n)
+	for i:=0; i<n; i++ {
+		ret[i] = iou.int()
+	}
+	return ret
+}
+func (iou *ioUtil) is(n int) []int {
+	return iou.ints(n)
+}
+func (iou *ioUtil) ints2(n int) (a, b []int) {
+	a = make([]int, n)
+	b = make([]int, n)
+	for i:=0; i<n; i++ {
+		a[i] = iou.int()
+		b[i] = iou.int()
+	}
+	return a, b
+}
+func (iou *ioUtil) is2(n int) (a, b []int) {
+	return iou.ints2(n)
+}
+func (iou *ioUtil) cumulativeSum(n int) (cumulative, ints []int) {
+	cumulative = append(cumulative, 0)
+	for i:=0; i<n; i++ {
+		ai := iou.int()
+		cumulative = append(cumulative, cumulative[i] + ai)
+		ints = append(ints, ai)
+	}
+	return
+}
+func (iou *ioUtil) cms(n int) (cumulative, ints []int) {
+	return iou.cumulativeSum(n)
+}
+func (iou *ioUtil) print(a ...interface{}) {
+	fmt.Fprint(iou.writer, a...)
+}
+func (iou *ioUtil) p(a ...interface{}) {
+	iou.print(a...)
+}
+func (iou *ioUtil) println(a ...interface{}) {
+	fmt.Fprintln(iou.writer, a...)
+}
+func (iou *ioUtil) pl(a ...interface{}) {
+	iou.println(a...)
+}
+func (iou *ioUtil) printIntSlice(s []int) {
+	for _, v := range s {
+		fmt.Fprint(iou.writer, v)
+	}
+	fmt.Fprintln(iou.writer)
+}
+func (iou *ioUtil) pis(s []int) {
+	iou.printIntSlice(s)
+}
+func (iou *ioUtil) printIntSliceSpace(s []int) {
+	last := len(s) - 1
+	for i, v := range s {
+		fmt.Fprint(iou.writer, v)
+		if i == last {
+			fmt.Fprintln(iou.writer)
+		} else {
+			fmt.Fprint(iou.writer, " ")
+		}
+	}
+}
+func (iou *ioUtil) piss(s []int) {
+	iou.printIntSliceSpace(s)
+}
+func (iou *ioUtil) flush() {
+	iou.writer.Flush()
+}
+func (iou *ioUtil) fl() {
+	iou.flush()
+}
+var iou = newIOUtil()
---
+++ b/260/b.go
@@ -0,0 +1,196 @@
+package main
+
+import(
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"sort"
+)
+
+func main() {
+	defer iou.fl()
+
+	n := iou.i()
+	x := iou.i()
+	y := iou.i()
+	z := iou.i()
+	a := iou.is(n)
+	b := iou.is(n)
+
+	s := make([]st, n)
+
+	for i:=0; i<n; i++ {
+		sti := st{i+1, a[i], b[i], a[i]+b[i], false}
+		s[i] = sti
+	}
+
+	sort.SliceStable(s, func(i, j int) bool {return s[i].a > s[j].a })
+	f(s, x)
+	sort.SliceStable(s, func(i, j int) bool {return s[i].i < s[j].i })
+	sort.SliceStable(s, func(i, j int) bool {return s[i].b > s[j].b })
+	f(s, y)
+	sort.SliceStable(s, func(i, j int) bool {return s[i].i < s[j].i })
+	sort.SliceStable(s, func(i, j int) bool {return s[i].c > s[j].c })
+	f(s, z)
+	sort.SliceStable(s, func(i, j int) bool {return s[i].i < s[j].i })
+
+	for _, v := range s {
+		if v.g {
+			iou.pl(v.i)
+		}
+	}
+}
+
+func f(s []st, x int) {
+	for i:=0; i<len(s); i++ {
+		if x == 0 {
+			return
+		}
+		if s[i].g {
+			continue
+		}
+		s[i].g = true
+		x--
+	}
+}
+
+type st struct {
+	i, a, b, c int
+	g bool
+}
+
+func Pow(x, n int) int {
+	ret := 1
+	for i:=0; i<n; i++ {
+		ret *= x
+	}
+	return ret
+}
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+func Max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func Min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+type ioUtil struct {
+	scanner *bufio.Scanner
+	reader *bufio.Reader
+	writer *bufio.Writer
+}
+func newIOUtil() *ioUtil {
+	iou := ioUtil{
+		scanner: bufio.NewScanner(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+	iou.scanner.Split(bufio.ScanWords)
+	iou.scanner.Buffer(make([]byte, 1024), math.MaxInt64)
+	return &iou
+}
+func (*ioUtil) toInt(s string) int {
+	ret, _ := strconv.Atoi(s)
+	return ret
+}
+func (iou *ioUtil) int() int {
+	iou.scanner.Scan()
+	return iou.toInt(iou.scanner.Text())
+}
+func (iou *ioUtil) str() string {
+	iou.scanner.Scan()
+	return iou.scanner.Text()
+}
+func (iou *ioUtil) i() int {
+	return iou.int()
+}
+func (iou *ioUtil) s() string {
+	return iou.str()
+}
+func (iou *ioUtil) ints(n int) []int {
+	ret := make([]int, n)
+	for i:=0; i<n; i++ {
+		ret[i] = iou.int()
+	}
+	return ret
+}
+func (iou *ioUtil) is(n int) []int {
+	return iou.ints(n)
+}
+func (iou *ioUtil) ints2(n int) (a, b []int) {
+	a = make([]int, n)
+	b = make([]int, n)
+	for i:=0; i<n; i++ {
+		a[i] = iou.int()
+		b[i] = iou.int()
+	}
+	return a, b
+}
+func (iou *ioUtil) is2(n int) (a, b []int) {
+	return iou.ints2(n)
+}
+func (iou *ioUtil) cumulativeSum(n int) (cumulative, ints []int) {
+	cumulative = append(cumulative, 0)
+	for i:=0; i<n; i++ {
+		ai := iou.int()
+		cumulative = append(cumulative, cumulative[i] + ai)
+		ints = append(ints, ai)
+	}
+	return
+}
+func (iou *ioUtil) cms(n int) (cumulative, ints []int) {
+	return iou.cumulativeSum(n)
+}
+func (iou *ioUtil) print(a ...interface{}) {
+	fmt.Fprint(iou.writer, a...)
+}
+func (iou *ioUtil) p(a ...interface{}) {
+	iou.print(a...)
+}
+func (iou *ioUtil) println(a ...interface{}) {
+	fmt.Fprintln(iou.writer, a...)
+}
+func (iou *ioUtil) pl(a ...interface{}) {
+	iou.println(a...)
+}
+func (iou *ioUtil) printIntSlice(s []int) {
+	for _, v := range s {
+		fmt.Fprint(iou.writer, v)
+	}
+	fmt.Fprintln(iou.writer)
+}
+func (iou *ioUtil) pis(s []int) {
+	iou.printIntSlice(s)
+}
+func (iou *ioUtil) printIntSliceSpace(s []int) {
+	last := len(s) - 1
+	for i, v := range s {
+		fmt.Fprint(iou.writer, v)
+		if i == last {
+			fmt.Fprintln(iou.writer)
+		} else {
+			fmt.Fprint(iou.writer, " ")
+		}
+	}
+}
+func (iou *ioUtil) piss(s []int) {
+	iou.printIntSliceSpace(s)
+}
+func (iou *ioUtil) flush() {
+	iou.writer.Flush()
+}
+func (iou *ioUtil) fl() {
+	iou.flush()
+}
+var iou = newIOUtil()
